52226b4ba06b0b07429b09acda3aba5249d30b33
Brahms ZOUMENOU <azoumenou@sodsoncorp.com>
2022-06-10 18:20:17 +0100

    Ajout d'un Projet web test

---
+++ MyAppAzureDevops/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyAppAzureDevops.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
